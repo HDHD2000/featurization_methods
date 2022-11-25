c873e12b3a09079eb2753c000da5d115e5db9fc4
HDHD2000 <47893466+HDHD2000@users.noreply.github.com>
2022-11-25 11:34:56 +0100

    Add files via upload

---
+++ Landscape_torus_sphere_with_noise.py
@@ -0,0 +1,53 @@
+from sklearn.preprocessing   import MinMaxScaler
+from sklearn.pipeline        import Pipeline
+from sklearn.svm             import SVC
+from sklearn.ensemble        import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+import gudhi as gd
+import gudhi.representations
+import matplotlib.pyplot as plt
+from ripser import Rips
+from gudhi.datasets.generators import points
+from sklearn.model_selection import train_test_split
+
+repetitions = 100
+
+nb_points = 100
+num_diag_per_class = 100
+
+mu, sigma = 0, 0.5
+
+test_score, train_score = [], []
+
+for _ in range(repetitions):
+    dgms, labs = [], []
+    for _ in range(num_diag_per_class):
+        labs.append(0)
+        sphere_points = points.sphere(n_samples = nb_points, ambient_dim = 3, radius = 1, sample = "random") + np.random.normal(mu, sigma, [nb_points,3]) 
+        AlphaSphere = gd.AlphaComplex(points = sphere_points).create_simplex_tree()
+        dgmSphere = AlphaSphere.persistence()
+        dgms.append(AlphaSphere.persistence_intervals_in_dimension(0))
+        
+    for _ in range(num_diag_per_class):
+        labs.append(1)
+        alpha = 2*np.pi*np.random.random(nb_points)
+        beta =  2*np.pi*np.random.random(nb_points) 
+        torus_points = np.column_stack(((0.5*np.cos(alpha)+1)*np.cos(beta), (0.5*np.cos(alpha)+1)*np.sin(beta), 0.5*np.sin(alpha))) + np.random.normal(mu, sigma, [nb_points,3])
+        AlphaTorus = gd.AlphaComplex(points = torus_points).create_simplex_tree()
+        dgmTorus = AlphaTorus.persistence()
+        dgms.append(AlphaTorus.persistence_intervals_in_dimension(0))
+        
+    train_labs, test_labs, train_dgms, test_dgms = train_test_split(labs, dgms)  
+    
+    pipe = Pipeline([("Separator", gd.representations.DiagramSelector(limit=np.inf, point_type="finite")),
+                     ("Scaler",    gd.representations.DiagramScaler(scalers=[([0,1], MinMaxScaler())])),
+                     ("TDA",       gd.representations.Landscape()),
+                     ("Estimator", SVC())])
+
+    model = pipe.fit(train_dgms, train_labs)
+    train_score.append(model.score(train_dgms, train_labs))
+    test_score.append(model.score(test_dgms,  test_labs))
+
+print('Average training score after ' + str(repetitions) + ' repetitions: ' + str(np.mean(train_score)))
+print('Average testing score after ' + str(repetitions) + ' repetitions: ' + str(np.mean(test_score)))
